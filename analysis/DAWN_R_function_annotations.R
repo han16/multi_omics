a82336aeebc98b737c8eab32c5335dcb3e9fe59f
han <shengtonghan@gmail.com>
2024-03-19 11:41:03 -0500

    3/19/2024

--- analysis/DAWN_R_function_annotations.R
+++ analysis/DAWN_R_function_annotations.R
@@ -386,3 +386,27 @@ graphicalModel_range <- function(dat,  primary_idx, lambda_min, lambda_max, lamb
   stopifnot(ncol(mat) == nrow(mat))
   (mat + t(mat))/2
 }
+
+
+#####################
+#> covarianceSelection::screen
+function (dat, pv, p_thres = 0.1, num_genes = 3500) 
+{
+  stopifnot(ncol(dat) == length(pv))
+  stopifnot(is.numeric(dat), is.matrix(dat))
+  n <- nrow(dat)
+  d <- ncol(dat)
+  cor_mat <- abs(stats::cor(dat))
+  primary <- which(pv < p_thres)
+  candidates <- c(1:d)[-primary]
+  if (length(candidates) == 0 | length(primary) >= num_genes) {
+    return(list(primary = primary, secondary = NA, cor_thres = NA))
+  }
+  cor_vec <- apply(cor_mat[candidates, primary], 1, max)
+  idx <- order(cor_vec, decreasing = T)[1:(num_genes - length(primary))]
+  cor_thres <- min(cor_vec[idx])
+  secondary <- candidates[idx]
+  stopifnot(length(intersect(primary, secondary)) == 0)
+  list(primary = sort(primary), secondary = sort(secondary), 
+       cor_thres = cor_thres)
+}
